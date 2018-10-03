2596417f8ad4de0f197c8b8de200f80576b1f9da
Adam P <adamparalovos@hotmail.com>
2018-10-03 16:05:46 -0400

    Add friend object to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,20 @@
-var friendsArray = [];
+var friendsArray = [
+    {
+        "name":"Adam",
+        "photo":"https://adam28p.github.io/img/profile.jpg",
+        "scores":[
+            5,
+            3,
+            2,
+            1,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3
+          ]
+      }
+];
 
 module.exports = friendsArray;
